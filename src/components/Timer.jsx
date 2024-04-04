19d0b3ba3a69db9f7ac2fa6ca1320df2499358e9
Andreson Souza <andresonjazz@gmail.com>
2024-04-04 19:23:44 -0300

    implemente a lógica para iniciar e parar o cronômetro

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import TimerDisplay from "./TimerDisplay";
 import TimerConstrols from "./TimerConstrols";
 import LapList from "./LapList";
-import "./Timer.css"
+import "./Timer.css";
 
 const Timer = () => {
+  const [milliseconds, setMilliseconds] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+  const [laps, setLaps] = useState([]);
+
+  const formatTime = () => {
+    const minutes = ("0" + Math.floor((milliseconds / 60000) % 60)).slice(-2);
+    const seconds = ("0" + Math.floor((milliseconds / 1000) % 60)).slice(-2);
+    const centiseconds = ("0" + Math.floor((milliseconds / 10) % 100)).slice(
+      -2
+    );
+
+    return `${minutes}:${seconds}:${centiseconds}`;
+  };
+
+  const startTimer = (interval) => {
+    return setInterval(() => {
+      setMilliseconds((prevMilliseconds) => prevMilliseconds + 10);
+    }, 10);
+  };
+
+  const stopTimer = (interval) => {
+    clearInterval(interval);
+    return interval;
+  };
+
+  useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = startTimer(interval);
+    } else if (!timerOn) {
+      interval = stopTimer(interval);
+    }
+    return () => stopTimer(interval);
+  }, [timerOn]);
+
   return (
     <div className="timer-container">
-      <TimerDisplay />
-      <TimerConstrols />
+      <TimerDisplay time={formatTime()} />
+      <TimerConstrols
+        onStart={() => setTimerOn(true)}
+        onStop={() => setTimerOn(false)}
+      />
       <LapList />
     </div>
   );
--- src/components/TimerConstrols.jsx
+++ src/components/TimerConstrols.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-const TimerConstrols = () => {
-  return <div className="timer-controls">
-    <button>Iniciar</button>
-    <button>Zerar</button>
-    </div>;
+const TimerConstrols = ({ onStart, onStop }) => {
+  return (
+    <div className="timer-controls">
+      <button onClick={onStart}>Iniciar</button>
+      <button onClick={onStop}>Zerar</button>
+    </div>
+  );
 };
 
 export default TimerConstrols;
